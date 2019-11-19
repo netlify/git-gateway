7ad2ca12b6ac621dabab0a77239e614411c3a14f
Marcus Weiner <marcus.weiner@gmail.com>
2019-11-19 11:01:13 +0100

    Remove debug log

--- api/gitlab.go
+++ api/gitlab.go
@@ -58,10 +58,7 @@ func gitlabDirector(r *http.Request) {
 	log := getLogEntry(r)
 
 	// remove header which causes false positives for blocking on Gitlab loadbalancers
-	if clientIP := r.Header.Get("Client-IP"); clientIP != "" {
-		log.WithField("client_ip", clientIP).Info("Detected client ip header present. Removing...")
-		r.Header.Del("Client-IP")
-	}
+	r.Header.Del("Client-IP")
 
 	config := getConfig(ctx)
 	tokenType := config.GitLab.AccessTokenType
